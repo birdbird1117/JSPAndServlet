b45f2df5319c118a29d1dafb7f3cac38dbad1b9f
birdbird1117 <dapeng.han.1989@gmail.com>
2018-10-14 23:36:29 +0200

    mvc simple

---
+++ JSPAndServletDemo/servletdemo/src/com/code/servletdemo/MvcDemoServlet.java
@@ -0,0 +1,52 @@
+package com.code.servletdemo;
+
+import java.io.IOException;
+
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MvcDemoServlet
+ */
+@WebServlet("/MvcDemoServlet")
+public class MvcDemoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MvcDemoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// step0: add data
+		String[] students = {"john0", "john1", "john2"};
+		request.setAttribute("student_list", students);
+		
+		// step1: get dispatcher
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/view_students.jsp");
+		
+		// step2: forward the request to JSP
+		dispatcher.forward(request, response);
+			
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
